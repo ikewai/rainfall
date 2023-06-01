fa52a7233fb2e733adbc6473a2b5c2a0f3054ce7
mdodge2 <mdodge@hawaii.edu>
2023-06-01 08:35:37 -1000

    add year-monthly data get

--- code/monthly/bash/monthly_rf_wget.sh
+++ code/monthly/bash/monthly_rf_wget.sh
@@ -22,6 +22,7 @@ pathMaster=$'preliminary/rainfall/'
 urlMaster=$'https://ikeauth.its.hawaii.edu/files/v2/download/public/system/ikewai-annotated-data/HCDP/workflow_data/'
 
 #define files
+year_monthly_rf=$pathMaster'data_outputs/tables/station_data/monthly/partial_filled/statewide/Statewide_Partial_Filled_Daily_RF_mm_'$fileYear'.csv'
 month_daily_rf=$pathMaster'data_outputs/tables/station_data/daily/partial_filled/statewide/Statewide_Partial_Filled_Daily_RF_mm_'$fileDate'.csv'
 month_count_rf=$pathMaster'data_outputs/tables/rf_station_tracking/count/monthly/'$fileYear'_count_log_monthly_rf.csv'
 
@@ -29,8 +30,10 @@ month_count_rf=$pathMaster'data_outputs/tables/rf_station_tracking/count/monthly
 cd /home/hawaii_climate_products_container
 
 #wget files and put in proper dir locations
-echo "Getting data from $urlMaster$month_daily_rf"
+echo "Getting monthly data of this year from $urlMaster$year_monthly_rf"
+echo "Getting daily data of this month from $urlMaster$month_daily_rf"
 echo "Getting count from $urlMaster$month_count_rf"
+wget $urlMaster$year_monthly_rf -O './'#year_monthly_rf || rm $year_monthly_rf
 wget $urlMaster$month_daily_rf -O './'$month_daily_rf || rm $month_daily_rf
 wget $urlMaster$month_count_rf -O './'$month_count_rf || rm $month_count_rf
 
