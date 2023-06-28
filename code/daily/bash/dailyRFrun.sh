615f8c62002d285e6caef36f1553340eccf4a786
mdodge2 <mdodge@hawaii.edu>
2023-06-27 15:50:52 -1000

    remove unnecessary dailyRFrun.sh

--- code/daily/bash/dailyRFrun.sh
+++
@@ -1,8 +0,0 @@
-#run all daily rf r code in sequence
-bash /home/hawaii_climate_products_container/preliminary/rainfall/code/daily/bash/dailyRFwget.sh #wget all files
-Rscript /home/hawaii_climate_products_container/preliminary/rainfall/code/daily/rcode/hads_daily_rf_FINAL.R #hads agg
-Rscript /home/hawaii_climate_products_container/preliminary/rainfall/code/daily/rcode/nws_rr5_daily_rf_FINAL.R #nws agg
-Rscript /home/hawaii_climate_products_container/preliminary/rainfall/code/daily/rcode/madis_daily_rf_FINAL.R #madis agg
-Rscript /home/hawaii_climate_products_container/preliminary/rainfall/code/daily/rcode/all_data_daily_merge_table_rf_FINAL.R #merge all daily data
-Rscript /home/hawaii_climate_products_container/preliminary/rainfall/code/daily/rcode/qaqc_randfor_bad_data_flag_remove_rf_FINAL.R #qaqc daily rf
-Rscript /home/hawaii_climate_products_container/preliminary/rainfall/code/daily/rcode/daily_gap_fill_NR_only_rf_FINAL.R #gap fill daily rf
