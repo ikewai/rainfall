f6f249c08a8e559349a1f768f8d2271f6a8d9b01
Michael Dodge <mdodge@hawaii.edu>
2023-09-05 11:24:50 -1000

    switch to new version of daily wget

--- code/daily/bash/dailyRFwget.sh
+++ code/daily/bash/dailyRFwget.sh
@@ -1,32 +1,52 @@
 #!/bin/bash
+###BASH CODE TO DEFINE & WGET ALL FILES IF THEY EXIST 
 
-##wget datastream daily rf monthly files
-wget -v "https://ikeauth.its.hawaii.edu/files/v2/download/public/system/ikewai-annotated-data/HCDP/workflow_data/preliminary/rainfall/working_data/hads/$(AGGREGATION_DATE_YESTERDAY_Y_M)_hads_daily_rf.csv" -P /home/hawaii_climate_products_container/preliminary/rainfall/working_data/hads/
-wget -v "https://ikeauth.its.hawaii.edu/files/v2/download/public/system/ikewai-annotated-data/HCDP/workflow_data/preliminary/rainfall/working_data/madis/$(AGGREGATION_DATE_YESTERDAY_Y_M)_madis_daily_rf.csv" -P /home/hawaii_climate_products_container/preliminary/rainfall/working_data/madis/
-wget -v "https://ikeauth.its.hawaii.edu/files/v2/download/public/system/ikewai-annotated-data/HCDP/workflow_data/preliminary/rainfall/working_data/nws_rr5/$(AGGREGATION_DATE_YESTERDAY_Y_M)_nws_daily_rf.csv" -P /home/hawaii_climate_products_container/preliminary/rainfall/working_data/nws_rr5/
+echo "Starting Daily RF Pull"
+#define TZ as HST
+TZ=Pacific/Honolulu 
 
-##wget station monitoring log tables
-#missing/unknown stations monthly file
-wget -v "https://ikeauth.its.hawaii.edu/files/v2/download/public/system/ikewai-annotated-data/HCDP/workflow_data/preliminary/rainfall/data_outputs/tables/rf_station_tracking/missing/$(AGGREGATION_DATE_YESTERDAY_Y_M)_unknown_rf_sta.csv" -P /home/hawaii_climate_products_container/preliminary/rainfall/data_outputs/tables/rf_station_tracking/missing/
-#count log 
-wget -v "https://ikeauth.its.hawaii.edu/files/v2/download/public/system/ikewai-annotated-data/HCDP/workflow_data/preliminary/rainfall/data_outputs/tables/rf_station_tracking/count/$(AGGREGATION_DATE_YESTERDAY_Y_M)_count_log_daily_rf.csv" -P /home/hawaii_climate_products_container/preliminary/rainfall/data_outputs/tables/rf_station_tracking/count/
-#gap fill log
-wget -v "https://ikeauth.its.hawaii.edu/files/v2/download/public/system/ikewai-annotated-data/HCDP/workflow_data/preliminary/rainfall/data_outputs/tables/rf_station_tracking/gapFilling/$(AGGREGATION_DATE_YESTERDAY_Y_M)_gap_fill_log_daily_rf.csv" -P /home/hawaii_climate_products_container/preliminary/rainfall/data_outputs/tables/rf_station_tracking/gapFilling/
-#qaqc fail log
-wget -v "https://ikeauth.its.hawaii.edu/files/v2/download/public/system/ikewai-annotated-data/HCDP/workflow_data/preliminary/rainfall/data_outputs/tables/rf_station_tracking/qaqc_fail/$(AGGREGATION_DATE_YESTERDAY_Y_M)_qaqc_fail_daily_rf.csv" -P /home/hawaii_climate_products_container/preliminary/rainfall/data_outputs/tables/rf_station_tracking/qaqc_fail/
+#define filedate year_month of yesterday
+fileDate=$(date --date=$AGGREGATION_DATE_YESTERDAY +"%Y_%m") 
 
-##wget daily rf monthly station files 
-#rf station daily source
-wget -v "https://ikeauth.its.hawaii.edu/files/v2/download/public/system/ikewai-annotated-data/HCDP/workflow_data/preliminary/rainfall/data_outputs/tables/station_data/daily/source/statewide/Statewide_Daily_Source_$(AGGREGATION_DATE_YESTERDAY_Y_M).csv" -P /home/hawaii_climate_products_container/preliminary/rainfall/data_outputs/tables/station_data/daily/source/statewide/
-#rf station daily raw rainfall mm
-wget -v "https://ikeauth.its.hawaii.edu/files/v2/download/public/system/ikewai-annotated-data/HCDP/workflow_data/preliminary/rainfall/data_outputs/tables/station_data/daily/raw/statewide/Statewide_Raw_Daily_RF_mm_$(AGGREGATION_DATE_YESTERDAY_Y_M).csv" -P /home/hawaii_climate_products_container/preliminary/rainfall/data_outputs/tables/station_data/daily/raw/statewide/
-#rf station daily qaqc raw rainfall mm
-wget -v "https://ikeauth.its.hawaii.edu/files/v2/download/public/system/ikewai-annotated-data/HCDP/workflow_data/preliminary/rainfall/data_outputs/tables/station_data/daily/raw_qc/statewide/Statewide_QAQC_Raw_Daily_RF_mm_$(AGGREGATION_DATE_YESTERDAY_Y_M).csv" -P /home/hawaii_climate_products_container/preliminary/rainfall/data_outputs/tables/station_data/daily/raw_qc/statewide/
-#rf station daily qaqc flag
-wget -v "https://ikeauth.its.hawaii.edu/files/v2/download/public/system/ikewai-annotated-data/HCDP/workflow_data/preliminary/rainfall/data_outputs/tables/station_data/daily/qc_flag/statewide/Statewide_RF_QAQC_Daily_Bad_Flag_$(AGGREGATION_DATE_YESTERDAY_Y_M).csv" -P /home/hawaii_climate_products_container/preliminary/rainfall/data_outputs/tables/station_data/daily/qc_flag/statewide/
-#rf station daily qaqc prob
-wget -v "https://ikeauth.its.hawaii.edu/files/v2/download/public/system/ikewai-annotated-data/HCDP/workflow_data/preliminary/rainfall/data_outputs/tables/station_data/daily/qc_prob/statewide/Statewide_RF_QAQC_Daily_Bad_Prob_$(AGGREGATION_DATE_YESTERDAY_Y_M).csv" -P /home/hawaii_climate_products_container/preliminary/rainfall/data_outputs/tables/station_data/daily/qc_prob/statewide/
-#rf station daily partial filled rainfall mm
-wget -v "https://ikeauth.its.hawaii.edu/files/v2/download/public/system/ikewai-annotated-data/HCDP/workflow_data/preliminary/rainfall/data_outputs/tables/station_data/daily/partial_filled/statewide/Statewide_Partial_Filled_Daily_RF_mm_$(AGGREGATION_DATE_YESTERDAY_Y_M).csv" -P /home/hawaii_climate_products_container/preliminary/rainfall/data_outputs/tables/station_data/daily/partial_filled/
+#define master dir
+pathMaster=$'preliminary/rainfall/' 
 
+#define url
+urlMaster=$'https://ikeauth.its.hawaii.edu/files/v2/download/public/system/ikewai-annotated-data/HCDP/workflow_data/'
 
+#define files
+NWS_daily_rf=$pathMaster'working_data/nws_rr5/'$fileDate'_nws_daily_rf.csv'
+Hads_daily_rf=$pathMaster'working_data/hads/'$fileDate'_hads_daily_rf.csv'
+Madis_daily_rf=$pathMaster'working_data/madis/'$fileDate'_madis_daily_rf.csv'
+RF_source=$pathMaster'data_outputs/tables/station_data/daily/source/statewide/Statewide_Daily_Source_'$fileDate'.csv'
+RF_raw=$pathMaster'data_outputs/tables/station_data/daily/raw/statewide/Statewide_Raw_Daily_RF_mm_'$fileDate'.csv'
+QAQC_flag=$pathMaster'data_outputs/tables/station_data/daily/qc_flag/statewide/Statewide_RF_QAQC_Daily_Bad_Flag_'$fileDate'.csv'
+QAQC_prob=$pathMaster'data_outputs/tables/station_data/daily/qc_prob/statewide/Statewide_RF_QAQC_Daily_Bad_Prob_'$fileDate'.csv'
+QAQC_fail=$pathMaster'data_outputs/tables/rf_station_tracking/qaqc_fail/'$fileDate'_qaqc_fail_daily_rf.csv'
+QAQC_raw=$pathMaster'data_outputs/tables/station_data/daily/raw_qc/statewide/Statewide_QAQC_Raw_Daily_RF_mm_'$fileDate'.csv'
+RF_fill=$pathMaster'data_outputs/tables/station_data/daily/partial_filled/statewide/Statewide_Partial_Filled_Daily_RF_mm_'$fileDate'.csv'
+Fill_log=$pathMaster'data_outputs/tables/rf_station_tracking/gapFilling/'$fileDate'_gap_fill_log_daily_rf.csv'
+RF_unknown=$pathMaster'data_outputs/tables/rf_station_tracking/missing/'$fileDate'_unknown_rf_sta.csv'
+Count_log=$pathMaster'data_outputs/tables/rf_station_tracking/count/'$fileDate'_count_log_daily_rf.csv'
+Last_RF=$pathMaster'data_outputs/tables/rf_station_tracking/lastObs/lastRFdayObs.csv'
+
+#set dir
+cd /home/hawaii_climate_products_container
+
+#wget files and put in proper dir locations
+wget $urlMaster$NWS_daily_rf -O './'$NWS_daily_rf || rm $NWS_daily_rf
+wget $urlMaster$Hads_daily_rf -O './'$Hads_daily_rf || rm $Hads_daily_rf
+wget $urlMaster$Madis_daily_rf -O './'$Madis_daily_rf || rm $Madis_daily_rf
+wget $urlMaster$RF_source -O './'$RF_source || rm $RF_source
+wget $urlMaster$RF_raw -O './'$RF_raw || rm $RF_raw
+wget $urlMaster$QAQC_flag -O './'$QAQC_flag || rm $QAQC_flag   
+wget $urlMaster$QAQC_prob -O './'$QAQC_prob || rm $QAQC_prob   
+wget $urlMaster$QAQC_fail -O './'$QAQC_fail || rm $QAQC_fail   
+wget $urlMaster$QAQC_raw -O './'$QAQC_raw || rm $QAQC_raw      
+wget $urlMaster$RF_fill -O './'$RF_fill || rm $RF_fill
+wget $urlMaster$Fill_log -O './'$Fill_log || rm $Fill_log      
+wget $urlMaster$RF_unknown -O './'$RF_unknown || rm $RF_unknown
+wget $urlMaster$Count_log -O './'$Count_log || rm $Count_log   
+wget $urlMaster$Last_RF -O './'$Last_RF || rm $Last_RF
+
+##PAU##
